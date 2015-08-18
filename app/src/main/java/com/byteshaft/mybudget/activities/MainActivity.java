1881d76e2a554bb4e2e45bd9bf304af7ef94d4fc
s9iper1 <s9iper1@gmail.com>
2015-08-18 13:16:17 +0500

    set drawer burger

--- app/src/main/java/com/byteshaft/mybudget/activities/MainActivity.java
+++ app/src/main/java/com/byteshaft/mybudget/activities/MainActivity.java
@@ -2,13 +2,13 @@ package com.byteshaft.mybudget.activities;
 
 
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.app.DialogFragment;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView mDrawerList;
     private Fragment mFragment;
     private String[] mListTitles;
+    private int mPositionGlobal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +36,10 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
         tx.replace(R.id.container, new HomeFragment());
         tx.commit();
+        getSupportActionBar().setElevation(0);
+        setTheme(R.style.MyAppTheme);
         setContentView(R.layout.mainactivity);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerToggle = getActionBarDrawerToggle();
         mDrawerLayout.setDrawerListener(mDrawerToggle);
@@ -48,24 +52,38 @@ public class MainActivity extends AppCompatActivity {
         mDrawerList.setAdapter(new ArrayAdapter<>(this,
                 R.layout.drawer_list_item, mListTitles));
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        selectItem(0);
     }
 
     private ActionBarDrawerToggle getActionBarDrawerToggle() {
-        return new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open,
+        return new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open,
                 R.string.drawer_close) {
 
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Menu");
                 invalidateOptionsMenu();
             }
 
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
+                getSupportActionBar().setTitle(mListTitles[mPositionGlobal]);
                 invalidateOptionsMenu();
             }
         };
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
     }
 
     void newFragment(int position) {
@@ -80,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
                 mFragment = new ContactFragment();
                 break;
             default:
+                mFragment = new HomeFragment();
                 return;
         }
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -96,6 +115,7 @@ public class MainActivity extends AppCompatActivity {
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            mPositionGlobal = position;
             selectItem(position);
             newFragment(position);
             mDrawerLayout.closeDrawer(mDrawerList);
