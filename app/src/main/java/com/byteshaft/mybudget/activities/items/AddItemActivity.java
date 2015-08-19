f9a93a655da54d5cc768a9861e7c88ecaf9c3bca
scorpion01 <shgr8rb@gmail.com>
2015-08-19 13:24:17 +0500

    change some strings

--- app/src/main/java/com/byteshaft/mybudget/activities/items/AddItemActivity.java
+++ app/src/main/java/com/byteshaft/mybudget/activities/items/AddItemActivity.java
@@ -26,6 +26,7 @@ public class AddItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+        setTitle(R.string.activity_title);
         myDb = DBHelper.getInstance(this);
     }
 
