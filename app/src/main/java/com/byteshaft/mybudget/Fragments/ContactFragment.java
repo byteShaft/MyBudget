fa41d93afa9b4a5486b11c9c38ead6e2bffe9af4
scorpion01 <shgr8rb@gmail.com>
2015-08-24 18:33:37 +0500

    finalizing

--- app/src/main/java/com/byteshaft/mybudget/Fragments/ContactFragment.java
+++ app/src/main/java/com/byteshaft/mybudget/Fragments/ContactFragment.java
@@ -18,16 +18,15 @@ public class ContactFragment extends Fragment implements View.OnClickListener{
 
     private View baseView;
     private EditText mEditTextName;
-    private EditText mEditTextEmail;
     private EditText mEditTextMessage;
     private Button mButton;
+    private String emailAddress = "contact@amifinancialsolutions.ie";
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         baseView = inflater.inflate(R.layout.contact_fragment, container, false);
         mEditTextName = (EditText) baseView.findViewById(R.id.editText);
-        mEditTextEmail = (EditText) baseView.findViewById(R.id.editTextEmail);
         mEditTextMessage = (EditText) baseView.findViewById(R.id.editTextMessage);
         mButton = (Button) baseView.findViewById(R.id.button);
         mButton.setOnClickListener(this);
@@ -39,24 +38,19 @@ public class ContactFragment extends Fragment implements View.OnClickListener{
         switch (v.getId()) {
             case R.id.button:
                 String name = mEditTextName.getText().toString();
-                String email = mEditTextEmail.getText().toString();
                 String message = mEditTextMessage.getText().toString();
                 if (name.isEmpty()) {
                     Toast.makeText(getActivity(), "Please Enter Name", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (email.isEmpty()) {
-                    Toast.makeText(getActivity(), "please enter email", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 if (message.isEmpty()) {
                     Toast.makeText(getActivity(), "please enter Message", Toast.LENGTH_SHORT).show();
                 }
 
-                if (!name.isEmpty() && !email.isEmpty() && !message.isEmpty()) {
+                if (!name.isEmpty() &&  !message.isEmpty()) {
                     Intent intent = new Intent(Intent.ACTION_SEND);
                     intent.setType("message/rfc822");
-                    intent.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
+                    intent.putExtra(Intent.EXTRA_EMAIL, new String[]{emailAddress});
                     intent.putExtra(Intent.EXTRA_SUBJECT, name);
                     intent.putExtra(Intent.EXTRA_TEXT, message);
                     try {
